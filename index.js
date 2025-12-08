9548019e8cc0e80db548d820d0342d7be9305001
Mouaz Jamal <mj225ih@student.lnu.se>
2025-12-08 23:06:14 +0100

    Looping throug the ingrediens and messures

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ submit.addEventListener('click', () => {
     .then(res => res.json())
     .then(data => {
         const meal = data.meals[0]
+
+        const ingredients = []
+        for (let i = 0; i <= 20; i++) {
+         const ingredient = meal[`strIngredient${i}`]
+         const measure = meal[`strMeasure${i}`]
+
+         if (ingredient && ingredient.trim() !== '') {
+            ingredients.push(`${measure.trim()} ${ingredient}`)
+         }
+        }
+
         container.innerHTML = `
         <h2>${meal.strMeal}</h2>
         <img src="${meal.strMealThumb}" alt="The meal picture">
         <p>${meal.strInstructions}</p>
+        <br>
+        <p>${ingredients}</p>
         `
+        console.log(data)
     })
     .catch(err => console.error(err))
 })
