58bea4d0fab08460f2aa8f5d83eece4d5054b0c3
Mouaz Jamal <mj225ih@student.lnu.se>
2025-12-08 22:34:41 +0100

    For now adding the meal name pic and instructions with no css, but doing it later

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ submit.addEventListener('click', () => {
     .then(res => res.json())
     .then(data => {
         const meal = data.meals[0]
-        
+        container.innerHTML = `
+        <h2>${meal.strMeal}</h2>
+        <img src="${meal.strMealThumb}" alt="The meal picture">
+        <p>${meal.strInstructions}</p>
+        `
     })
     .catch(err => console.error(err))
 })
