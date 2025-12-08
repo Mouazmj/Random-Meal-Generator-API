4623e80edf6a89e0e939d38f10c91a0cee88a913
Mouaz Jamal <mj225ih@student.lnu.se>
2025-12-08 22:08:50 +0100

    Adding a loop for my api and getting the container selector

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const submit = document.getElementById('generate-btn')
+const container = document.querySelector('.container')
 
 submit.addEventListener('click', () => {
     fetch("https://www.themealdb.com/api/json/v1/1/random.php")
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+       data.foreach(item => {
+
+       })
     })
     .catch(err => console.error(err))
 })
