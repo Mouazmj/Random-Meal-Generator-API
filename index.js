9baafd97c198a2da88c42d75bf02acad9db037cd
Mouaz Jamal <mj225ih@student.lnu.se>
2025-12-09 16:26:40 +0100

    Made some changes trying to send my ingrdients as a ul, nut aint working now so im moving my container up

--- index.js
+++ index.js
@@ -8,22 +8,30 @@ submit.addEventListener('click', () => {
         const meal = data.meals[0]
 
         const ingredients = []
-        for (let i = 0; i <= 20; i++) {
-         const ingredient = meal[`strIngredient${i}`]
-         const measure = meal[`strMeasure${i}`]
-
-         if (ingredient && ingredient.trim() !== '') {
-            ingredients.push(`${measure.trim()} ${ingredient}`)
-         }
-        }
 
         container.innerHTML = `
         <h2>${meal.strMeal}</h2>
         <img src="${meal.strMealThumb}" alt="The meal picture">
         <p>${meal.strInstructions}</p>
-        <br>
-        <p>${ingredients}</p>
+        <li class="ingrediens-li">
+        
+        </li>
         `
+        
+        for (let i = 1; i <= 20; i++) {
+         const ingredient = meal[`strIngredient${i}`]
+         const measure = meal[`strMeasure${i}`]
+
+         if (ingredient && ingredient.trim() !== '') {
+            ingredients.push(`${measure.trim()} ${ingredient}`)
+            const ul = document.createElement('ul')
+            ul.innerText = ingredients[i]
+            const theList = document.querySelector('.ingrediens-li')
+            theList.appendChild(ul)
+         }
+        }
+
+        
         console.log(data)
     })
     .catch(err => console.error(err))
